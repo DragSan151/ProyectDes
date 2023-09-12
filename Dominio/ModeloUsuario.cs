e14c5aed9ea26b84acf2895110275d6169bfe76e
DragSan151 <93496727+DragSan151@users.noreply.github.com>
2023-09-12 11:54:01 -0600

    a

---
+++ Dominio/ModeloUsuario.cs
@@ -0,0 +1,12 @@
+﻿using Datos;
+namespace Dominio
+{
+    public class ModeloUsuario
+    {
+        UsuarioDatos usuarioDatos = new UsuarioDatos();
+        public bool LoginUser(string user, string cedula)
+        {
+            return usuarioDatos.Login(user, cedula);
+        }
+    }
+}
